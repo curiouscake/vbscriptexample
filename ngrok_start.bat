caf6366c11f2b8b76e66f40defba2ea945be0151
Curious Cake <Curiouscake@gmail.com>
2019-09-30 16:58:03 +0530

    Initial commit

---
+++ ngrok_start.bat
@@ -0,0 +1,2 @@
+C:\Users\Public\eclipse\ngrok.exe authtoken 1RYNtlrUN4bddj6LhNxC8apob0I_43aUJcA7b8QxotqAq5o7p
+C:\Users\Public\eclipse\ngrok.exe http 11.0.0.3:80
---
+++ run_test.bat
@@ -0,0 +1,3 @@
+REM c2986595@urhen.com
+REM ./ngrok authtoken 1RYNtlrUN4bddj6LhNxC8apob0I_43aUJcA7b8QxotqAq5o7p
+wscript.exe "C:\Users\Public\eclipse\inv.vbs" "C:\Users\Public\eclipse\ngrok_start.bat"
