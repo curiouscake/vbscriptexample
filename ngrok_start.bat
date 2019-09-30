6a8d6f4024b5a1747e6b401c78e54c81dc8e4906
Curious Cake <Curiouscake@gmail.com>
2019-09-30 17:23:14 +0530

    update

--- ngrok_start.bat
+++ ngrok_start.bat
@@ -1,2 +1,3 @@
-C:\Users\Public\eclipse\ngrok.exe authtoken 1RYNtlrUN4bddj6LhNxC8apob0I_43aUJcA7b8QxotqAq5o7p
+REM C:\Users\Public\eclipse\ngrok.exe authtoken 1RYNtlrUN4bddj6LhNxC8apob0I_43aUJcA7b8QxotqAq5o7p
+C:\Users\Public\eclipse\ngrok.exe authtoken 1RYW2plNyvS31XxL31acnoEZJnU_7pYJeBXb4TNixERKjo1Cs
 C:\Users\Public\eclipse\ngrok.exe http 11.0.0.3:80
--- run_test.bat
+++ run_test.bat
@@ -1,4 +1,5 @@
 REM c2986595@urhen.com
 REM curiouscake08@gmail.com
 REM ./ngrok authtoken 1RYNtlrUN4bddj6LhNxC8apob0I_43aUJcA7b8QxotqAq5o7p
+REM ./ngrok authtoken 1RYW2plNyvS31XxL31acnoEZJnU_7pYJeBXb4TNixERKjo1Cs
 wscript.exe "C:\Users\Public\eclipse\inv.vbs" "C:\Users\Public\eclipse\ngrok_start.bat"
