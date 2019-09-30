aeff67bf8244e2ccc642477d3d3d74635ef6423e
Curious Cake <Curiouscake@gmail.com>
2019-09-30 16:59:47 +0530

    Update

--- run_test.bat
+++ run_test.bat
@@ -1,3 +1,4 @@
 REM c2986595@urhen.com
+REM curiouscake08@gmail.com
 REM ./ngrok authtoken 1RYNtlrUN4bddj6LhNxC8apob0I_43aUJcA7b8QxotqAq5o7p
 wscript.exe "C:\Users\Public\eclipse\inv.vbs" "C:\Users\Public\eclipse\ngrok_start.bat"
